9d1c4cf428606970420b561dd48397d3fb5cb957
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-07-20 19:57:16 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 296 ms, Memory: 21944 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cs
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Text;
 
+//행 열 입력
 string[] inputs = Console.ReadLine().Split(' ');
-
 int COL = int.Parse(inputs[0]); int ROW = int.Parse(inputs[1]);
-//정의
 int[,] map = new int[COL, ROW]; int[,] arr = new int[COL, ROW];
-Queue<coordination> queue = new Queue<coordination>();
-coordination current = new coordination();
-int[] dy = { 0, 1, 0, -1 }; int[] dx = { 1, 0, -1, 0 };
-bool IsInside(int ny, int nx) => (0 <= ny && 0 <= nx && ny < COL && nx < ROW);
 
-//맵 저장
-Input();
+//맵 입력
+Queue<coordination> queue = new Queue<coordination>();
+for (int i = 0; i < COL; i++)
+{
+    inputs = Console.ReadLine().Split(' ');
+    for (int j = 0; j < ROW; j++) map[i, j] = int.Parse(inputs[j]);
+}
 for (int i = 0; i < COL; i++)
 {
     for (int j = 0; j < ROW; j++)
@@ -20,58 +20,47 @@ for (int i = 0; i < COL; i++)
         if (map[i, j] == (int)state.passable)
         {
             arr[i, j] = (int)state.unvisited;
-        } else if (map[i, j] == (int)state.departure)
+        } 
+        else if (map[i, j] == (int)state.departure)
         {
-            current.y = i; current.x = j;
-            arr[current.y, current.x] = 0;
+            queue.Enqueue(new coordination { x = j, y = i});
+            arr[i, j] = 0; //시작지점
         }
     }
 }
 
 //메인
-queue.Enqueue(current);
-while(queue.Count > 0)
+int[] dy = { 0, 1, 0, -1 }; int[] dx = { 1, 0, -1, 0 };
+bool IsInside(int ny, int nx) => (0 <= ny && 0 <= nx && ny < COL && nx < ROW);
+while (queue.Count > 0)
 {
-    current = queue.Dequeue();
+    coordination current = queue.Dequeue();
     for (int i = 0; i < 4; i++)
     {
         int ny = current.y + dy[i]; int nx = current.x + dx[i];
         if (IsInside(ny, nx) && map[ny, nx] == (int)state.passable)
         {
-            if(arr[ny, nx] == (int)state.unvisited || arr[ny, nx] > arr[current.y, current.x] + 1)
+            if(arr[ny, nx] == (int)state.unvisited)
             {
                 arr[ny, nx] = arr[current.y, current.x] + 1;
-                coordination next;
-                next.y = ny ; next.x = nx;
-                queue.Enqueue(next);
+                queue.Enqueue(new coordination { x = nx, y = ny });
             }
         }
     }
 }
-display();
 
-void Input()
+//출력
+StringBuilder output = new StringBuilder();
+
+for (int i = 0; i < COL; i++)
 {
-    for (int i = 0; i < COL; i++)
+    for (int j = 0; j < ROW; j++)
     {
-        inputs = Console.ReadLine().Split(' ');
-        for (int j = 0; j < ROW; j++) map[i, j] = int.Parse(inputs[j]);
+        output.Append(arr[i, j]).Append(" ");
     }
+    output.AppendLine();
 }
+Console.Write(output.ToString());
 
-void display()
-{
-    StringBuilder output = new StringBuilder();
-
-    for (int i = 0; i < COL; i++)
-    {
-        for (int j = 0; j < ROW; j++)
-        {
-            output.Append(arr[i, j]).Append(" ");
-        }
-        output.AppendLine();
-    }
-    Console.Write(output.ToString());
-}
 struct coordination { public int y; public int x; }
 enum state { unvisited = -1, obstacle, passable, departure }
