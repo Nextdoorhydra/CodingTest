f07785abb97aa34217a5e4671e832d73cbc4f643
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-07-23 07:15:48 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 2032 ms, Memory: 6512 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cs
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cs
@@ -1,14 +1,15 @@
 using System;
+using System.Text;
 
 string[] inputs = Console.ReadLine().Split(" ");
 int N = int.Parse(inputs[0]); int K = int.Parse(inputs[1]);
 
 int now = 1;
-int[] table = new int[N];
-List<int> per = new List<int>();
-for (int i = 0; i < N; i++) table[i] = now++;
+int[] table = new int[N]; 
+for (int i = 0; i < N; i++) table[i] = now++; now = 0;
 
-now = 0;
+List<int> per = new List<int>();
+Func<int, int> idx = (now) => { int n = now == 0 ? N - 1 : now - 1; return n; };
 while(per.Count < N)
 {
     int i = 0;
@@ -17,9 +18,10 @@ while(per.Count < N)
         if (table[now] != 0) i++;
         now = ++now % N;
     }
-    per.Add(table[now == 0 ? N - 1 : now - 1]);
-    table[now == 0 ? N - 1 : now - 1] =0;
+    per.Add(table[idx(now)]);
+    table[idx(now)] = 0;
 }
-Console.Write("<");
-for(int i = 0; i < N - 1; i++) Console.Write(per[i] + ", ");
-Console.WriteLine(per[per.Count - 1] + ">");
+
+StringBuilder sb = new StringBuilder();
+sb.Append("<"); for(int i = 0; i < N - 1; i++) sb.Append(per[i] + ", "); sb.Append(per[per.Count - 1] + ">");
+Console.WriteLine(sb);
