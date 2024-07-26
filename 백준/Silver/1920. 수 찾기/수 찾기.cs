369fa9cb528cdf1db12d03a05dcc697974d6cf8d
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-07-27 04:12:42 +0900

    [Silver IV] Title: 수 찾기, Time: 164 ms, Memory: 26496 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cs
+++ 백준/Silver/1920. 수 찾기/수 찾기.cs
@@ -5,12 +5,12 @@ int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 int M = int.Parse(Console.ReadLine());
 int[] ans = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
 
-HashSet<int> hashSet = new HashSet<int>(arr);
+Array.Sort(arr);
 StringBuilder sb = new StringBuilder();
 
 for (int i = 0; i < M; i++)
 {
-    if (hashSet.Contains(ans[i]))
+    if (binaryMatch(arr, ans[i]))
     {
         sb.Append("1\n");
     }
@@ -20,3 +20,25 @@ for (int i = 0; i < M; i++)
     }
 }
 Console.WriteLine(sb.ToString());
+
+bool binaryMatch(int[] arr, int n)
+{
+    int left = 0, right = N - 1;
+    while (true)
+    {
+        int mid = (left + right) / 2;
+        if (arr[mid] == n)
+        {
+            return true;
+        }
+        else if (arr[mid] < n)
+        {
+            left = mid + 1;
+        }
+        else if(arr[mid] > n)
+        {
+            right = mid - 1;
+        }
+        if (left > right) return false;
+    }
+}
