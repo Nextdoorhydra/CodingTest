82f79601f0ed603ab9f0060cefe32252f574f2b3
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-07-28 08:51:04 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 76 ms, Memory: 6128 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cs
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cs
@@ -1,32 +1,24 @@
 using System.Text;
 
 StringBuilder sb = new StringBuilder();
-
 int rep = int.Parse(Console.ReadLine());
-
 int[,] arr = new int[rep, 2];
 
 for(int i = 0; i < rep; i++)
-
 {
-    arr[i, 0] = int.Parse(Console.ReadLine());
-    arr[i, 1] = int.Parse(Console.ReadLine());
+    arr[i, 0] = int.Parse(Console.ReadLine()); arr[i, 1] = int.Parse(Console.ReadLine());
 }
 
-for(int i = 0; i < rep; i++)
-{
-    sb.Append($"{recur(arr[i, 0], arr[i, 1])}\n");
-}
-
-Console.WriteLine(sb.ToString());
-
+int[,] memo = new int[15, 15];
+for(int i = 0; i < 15; i++) memo[0, i] = i + 1;
 int recur(int f, int n)
 {
-    int sum = 0;
-    if (f == 0) return n;
-    for(int i = 1; i <= n; i++)
-    {
-        sum += recur(f - 1, i);
-    }
-    return sum;
+    if(memo[f, n - 1] != 0) return memo[f, n - 1];
+    if (f == 0) return memo[0, n - 1];
+    memo[f, n - 1] = Enumerable.Range(1, n).Select(i => recur(f - 1, i)).Sum();
+    return memo[f, n - 1];
 }
+
+for (int i = 0; i < rep; i++) sb.Append($"{recur(arr[i, 0], arr[i, 1])}\n");
+
+Console.WriteLine(sb.ToString());
