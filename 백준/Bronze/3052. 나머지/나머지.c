ed71513c3a51c27d53a1a3810491dfffed7d50da
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-07-29 18:35:10 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.c
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -2,16 +2,13 @@
 #include<stdint.h>
 
 int main() {
-    uint64_t log = 0;
+    uint64_t log = 0, pos;
     int ans = 0;
     for (int i = 0; i < 10; i++) {
         int num;
         scanf("%d", &num);
-        uint64_t pos = 1ULL << (num % 42);
-        if ((log & pos) == 0ULL) {
-            ans++;
-            log |= pos;
-        }
+        pos = 1ULL << (num % 42);
+        if ((log & pos) == 0ULL && ++ans) log |= pos;
     }
     printf("%d", ans);
     return 0;
