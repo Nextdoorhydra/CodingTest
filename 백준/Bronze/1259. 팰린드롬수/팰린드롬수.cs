a33bd859f4715451f494f55fb5ea9f382a6de98c
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-07-31 13:45:42 +0900

    [Bronze I] Title: 팰린드롬수, Time: 64 ms, Memory: 5364 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cs
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cs
@@ -1,10 +1,1 @@
-while (true)
-{
-    var s = Console.ReadLine();
-    if (s == "0") return;
-
-    int l = 0, r = s.Length - 1;
-    while (l < r) if (s[l++] != s[r--]) r = -1;
-
-    Console.WriteLine(r != -1 ? "yes" : "no");
-}
+while(true){var s=Console.ReadLine();if(s=="0") return;int l=0,r=s.Length-1;while(l<r)if(s[l++]!=s[r--])r=-1;Console.WriteLine(r!=-1?"yes":"no");}
