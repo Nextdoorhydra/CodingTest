283e37235cba8441102739ad08673edfe9b575c4
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-01 09:03:31 +0900

    [Gold III] Title: 세 용액, Time: 132 ms, Memory: 6984 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.cs
+++ 백준/Gold/2473. 세 용액/세 용액.cs
@@ -1,70 +1,31 @@
-using static System.Math;
-
 int gLength = int.Parse(Console.ReadLine());
 var g = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
 
-int l = 0, r = gLength - 1;
-Answer ans = new Answer() { n = long.MaxValue };
+long min = long.MaxValue;
+long[] ans = new long[3] { g[0], g[1], g[2] };
 
 Array.Sort(g);
-for(; l < gLength - 2; l++)
-{
-    for(r = gLength - 1; r - l > 1; r--)
-    {
-        long sum = g[l] + g[r];
-        long k = binaryMatch(-sum, l + 1, r - 1);
-        sum += k;
 
-        if (Abs(sum) < Abs(ans.n))
+for (int i = 0; i < gLength - 2; i++)
+{
+    int l = i + 1;
+    int r = gLength - 1;
+
+    while(l < r)
+    {
+        long sum = g[i] + g[l] + g[r];
+
+        var S = Math.Abs(sum);
+        if (S < min)
         {
-            ans.n = sum;
-            ans.nArr = new long[3] { g[l], g[r], k };
+            min = S;
+            ans = new long[3] { g[i], g[l], g[r] };
         }
+
+        if (sum == 0) goto OUT;
+        if (sum > 0) r--;
+        if (sum < 0) l++;
     }
 }
 
-Array.Sort(ans.nArr);
-Console.WriteLine($"{ans.nArr[0]} {ans.nArr[1]} {ans.nArr[2]}");
-
-long binaryMatch(long n, int left, int right)
-{
-    int preLeft = left;
-    int preRight = left;
-
-    while (true)
-    {
-        int mid = (left + right) / 2;
-
-        if (g[mid] == n)
-        {
-            return g[mid];
-        }
-        else if (g[mid] < n)
-        {
-            left = mid + 1;
-        }
-        else if (g[mid] > n)
-        {
-            right = mid - 1;
-        }
-
-        if (left > right)
-        {
-            long a = mid > preLeft ? Abs(g[mid - 1] - n) : long.MaxValue;
-            long b = Abs(g[mid] - n);
-            long c = mid < preRight ? Abs(g[mid + 1] - n) : long.MaxValue;
-
-            long min = Min(a, Min(b, c));
-
-            if(min == a) return g[mid - 1];
-            if(min == b) return g[mid];
-            if(min == c) return g[mid + 1];
-        }
-    }
-}
-
-struct Answer
-{
-    public long n;
-    public long[] nArr;
-}
+OUT: Console.WriteLine($"{ans[0]} {ans[1]} {ans[2]}");
