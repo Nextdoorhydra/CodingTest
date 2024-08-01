3501ba0f09dde86ac3ba39715ca7e8927006c5e4
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-01 14:18:14 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 956 ms, Memory: 162636 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cs
@@ -1,28 +1,14 @@
-using System.Text;
-
-StringBuilder sb = new StringBuilder();
-
 int size1 = int.Parse(Console.ReadLine());
 var arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 int size2 = int.Parse(Console.ReadLine());
 var arr2 = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
+int[] ans = new int[size2];
 var number = new int[20000001];
 
-Func<int, int> In = x => x >= 0 ? x : 10000000 - x; //10000000 부터 -1, -2, ...
+Func<int, int> In = x => x >= 0 ? x : 10000000 - x;
 
-for (int i = 0; i < size1; i++)
-{
-    int idx = In(arr[i]);
-    number[idx]++;
-}
-
-int[] ans = new int[size2];
-
-for (int i = 0; i < size2; i++)
-{
-    int idx = In(arr2[i]);
-    ans[i] = number[idx];
-}
+for (int i = 0; i < size1; i++) number[In(arr[i])]++;
+for (int i = 0; i < size2; i++) ans[i] = number[In(arr2[i])];
 
 Console.WriteLine(string.Join(" ", ans));
