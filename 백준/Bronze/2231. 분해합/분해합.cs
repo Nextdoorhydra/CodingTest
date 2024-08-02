a77158d49e5f4b7df41cb766361a6f32904400fe
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-02 13:08:05 +0900

    [Bronze II] Title: 분해합, Time: 152 ms, Memory: 18548 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cs
+++ 백준/Bronze/2231. 분해합/분해합.cs
@@ -1,13 +1,6 @@
 int ans = 0;
 var n = int.Parse(Console.ReadLine());
 
-while(ans++ < n)
-{
-    if (ans + ans.ToString().Sum(x => x - '0') == n)
-    {
-        Console.WriteLine(ans);
-        return;
-    }
-}
+for(; ans < n; ans++) if (ans + ans.ToString().Sum(x => x - '0') == n) break;
 
-Console.WriteLine(0);
+Console.WriteLine(ans < n ? ans : 0);
