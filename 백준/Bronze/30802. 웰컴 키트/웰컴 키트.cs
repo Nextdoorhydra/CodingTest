3bcc2f6bbf4c0bedefcbad58db70a8a16308b658
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-02 12:39:52 +0900

    [Bronze III] Title: 웰컴 키트, Time: 52 ms, Memory: 5832 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cs
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.cs
@@ -1,9 +1,10 @@
-int n = int.Parse(Console.ReadLine());
-var g = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-var pair = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+using static System.Console;
+
+int n = int.Parse(ReadLine());
+var g = Array.ConvertAll(ReadLine().Split(), int.Parse);
+var p = Array.ConvertAll(ReadLine().Split(), int.Parse);
 
 int cnt = 0;
-for (int i = 0; i < g.Length; i++) cnt += (g[i] % pair[0] == 0 ? 0 : 1) + g[i] / pair[0];
+for (int i = 0; i < 6; i++) cnt += (g[i] % p[0] == 0 ? 0 : 1) + g[i] / p[0];
 
-Console.WriteLine($"{cnt}");
-Console.WriteLine($"{n / pair[1]} {n % pair[1]}");
+WriteLine($"{cnt}\n{n / p[1]} {n % p[1]}");
