805c6c204d6241847a649556eb39859d2e220a9c
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-03 15:15:18 +0900

    [Silver II] Title: 랜선 자르기, Time: 64 ms, Memory: 6176 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cs
@@ -0,0 +1,22 @@
+var input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+var g = new long[input[0]];
+for (int i = 0; i < input[0]; i++) g[i] = int.Parse(Console.ReadLine());
+
+long ans = 0, l = 1, r = g.Max(); ;
+while (l <= r)
+{
+    long div = (l + r) / 2;
+    long det = g.Sum(x => x / div);
+
+    if(det >= input[1])
+    {
+        ans = Math.Max(ans, div);
+        l = div + 1;
+    }
+    else if(det < input[1])
+    {
+        r = div - 1;
+    }
+}
+
+Console.WriteLine(ans);
