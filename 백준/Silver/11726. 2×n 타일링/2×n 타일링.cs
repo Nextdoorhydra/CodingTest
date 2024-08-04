88f03655ef0279c384033dabff4b33e7cc68710d
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-04 11:44:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 52 ms, Memory: 5500 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cs
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cs
@@ -1,10 +1,8 @@
-using System.Numerics;
-
 int n = int.Parse(Console.ReadLine());
-var g = new BigInteger[1000];
+var g = new int[n + 1];
 
 g[0] = 1; g[1] = 2;
 
-for(int i = 2; i < n; i++) g[i] = g[i - 1] + g[i - 2];
+for(int i = 2; i < n; i++) g[i] = (g[i - 1] + g[i - 2]) % 10007;
 
-Console.WriteLine(g[n - 1] % 10007);
+Console.WriteLine(g[n - 1]);
