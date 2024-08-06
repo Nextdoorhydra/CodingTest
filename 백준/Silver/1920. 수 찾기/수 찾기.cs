d82b2bddb84b2149aad50a93f07ef773171a2379
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-06 16:49:14 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 29000 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cs
+++ 백준/Silver/1920. 수 찾기/수 찾기.cs
@@ -1,44 +1,7 @@
-using System.Text;
+using static System.Console;
 
-int N = int.Parse(Console.ReadLine());
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
-int M = int.Parse(Console.ReadLine());
-int[] ans = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+ReadLine();
+var g = Array.ConvertAll(ReadLine().Split(), int.Parse).ToHashSet();
 
-Array.Sort(arr);
-StringBuilder sb = new StringBuilder();
-
-for (int i = 0; i < M; i++)
-{
-    if (binaryMatch(arr, ans[i]))
-    {
-        sb.Append("1\n");
-    }
-    else
-    {
-        sb.Append("0\n");
-    }
-}
-Console.WriteLine(sb.ToString());
-
-bool binaryMatch(int[] arr, int n)
-{
-    int left = 0, right = N - 1;
-    while (true)
-    {
-        int mid = (left + right) / 2;
-        if (arr[mid] == n)
-        {
-            return true;
-        }
-        else if (arr[mid] < n)
-        {
-            left = mid + 1;
-        }
-        else if(arr[mid] > n)
-        {
-            right = mid - 1;
-        }
-        if (left > right) return false;
-    }
-}
+ReadLine();
+WriteLine(string.Join("\n", Array.ConvertAll(ReadLine().Split(), int.Parse).Select(x => g.Contains(x) ? 1 : 0)));
