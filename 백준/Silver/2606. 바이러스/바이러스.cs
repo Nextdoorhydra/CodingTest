f06e805e5b2b9592679f981fdb6086fda241f012
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-08 22:01:33 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 6428 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cs
+++ 백준/Silver/2606. 바이러스/바이러스.cs
@@ -1,9 +1,10 @@
 using static System.Console;
 
-int comCount = int.Parse(ReadLine());
+int c = int.Parse(ReadLine());
 int n = int.Parse(ReadLine());
-var visited = new bool[comCount]; visited[0] = true;
-var edge = Enumerable.Range(0, comCount).Select(_ => new List<int>()).ToList();
+
+var visited = new bool[c];
+var edge = Enumerable.Range(0, c).Select(_ => new List<int>()).ToList();
 
 for (int i = 0; i < n; i++)
 {
@@ -12,17 +13,17 @@ for (int i = 0; i < n; i++)
     edge[input[1]].Add(input[0]);
 }
 
-Queue<int> queue = new Queue<int>();
-queue.Enqueue(0);
+Queue<int> que = new Queue<int>();
+que.Enqueue(0);
 
-while (queue.Count > 0)
+while (que.Count > 0)
 {
-    int idx = queue.Dequeue();
+    int idx = que.Dequeue();
 
     var node = edge[idx];
     visited[idx] = true;
 
-    foreach (var p in node) if (!visited[p]) queue.Enqueue(p);
+    foreach (var p in node) if (!visited[p]) que.Enqueue(p);
 }
 
 WriteLine(visited.Count(x => x == true) - 1);
