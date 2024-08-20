170020e1503b1890bc65947ea0ae798d1662fe00
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-21 06:32:41 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 228 ms, Memory: 32160 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cs
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cs
@@ -1,31 +1,28 @@
-int size = int.Parse(Console.ReadLine());
-var ans = new int[size];
-var visited = new bool[size];
-var edgeList = Enumerable.Range(1, size).Select(x => new List<int>()).ToList();
-
-while (--size > 0)
+int n = int.Parse(Console.ReadLine());
+var ans = new int[n];
+var visited = new bool[n];
+var ls = Enumerable.Range(1, n).Select(x => new List<int>()).ToList();
+while (--n > 0)
 {
     var v = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-    edgeList[--v[0]].Add(--v[1]);
-    edgeList[v[1]].Add(v[0]);
+    ls[--v[0]].Add(--v[1]);
+    ls[v[1]].Add(v[0]);
 }
 
-Queue<int> queue = new Queue<int>(new int[1]);
+Queue<int> q = new Queue<int>(new int[1]);
 visited[0] = true;
-
-while (queue.Count > 0)
+while (q.Count > 0)
 {
-    var pos = queue.Dequeue();
+    var pos = q.Dequeue();
 
-    foreach(var v in edgeList[pos])
+    foreach(var v in ls[pos])
     {
         if (!visited[v])
         {
             visited[v] = true;
-            queue.Enqueue(v);
+            q.Enqueue(v);
             ans[v] = pos + 1;
         }
     }
 }
-
 Console.WriteLine(string.Join("\n", ans[1..]));
