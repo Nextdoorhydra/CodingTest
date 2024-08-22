7ef61982a86e465bc5f7904fa64df89546d5a9fd
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-22 19:57:16 +0900

    [Gold V] Title: Z, Time: 48 ms, Memory: 5536 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cs
+++ 백준/Gold/1074. Z/Z.cs
@@ -1,44 +1,29 @@
 var condi = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int k = (int)Math.Pow(2, condi[0]), answer = 0;
+int k = (int)Math.Pow(2, condi[0] - 1), answer = 0;
 
-recur(k / 4, k / 2, k / 2);
+recur(k / 2, k, k);
+Console.WriteLine(answer);
 
 void recur(int n, int row, int col)
 {
-    var IsEnd = (int r, int c) =>
-    {
-        if (r == condi[1] && c == condi[2]) Console.WriteLine(answer);
-        answer++;
-    };
+    int cnt = n * n * 4;
 
-    if (n == 0)
+    switch (row <= condi[1], col <= condi[2])
     {
-        IsEnd(row - 1, col - 1);
-        IsEnd(row - 1, col);
-        IsEnd(row, col - 1);
-        IsEnd(row, col);
-    }
-    else
-    {
-        int cnt = n * n * 4;
-
-        switch (row <= condi[1], col <= condi[2])
-        {
-            case (false, false):
-                recur(n / 2, row - n, col - n);
-                break;
-            case (false, true):
-                answer += cnt;
-                recur(n / 2, row - n, col + n);
-                break;
-            case (true, false):
-                answer += 2 * cnt;
-                recur(n / 2, row + n, col - n);
-                break;
-            case (true, true):
-                answer += 3 * cnt;
-                recur(n / 2, row + n, col + n);
-                break;
-        }
+        case (false, false):
+            if(n != 0) recur(n / 2, row - n, col - n);
+            break;
+        case (false, true):
+            answer += n == 0 ? 1 : cnt; 
+            if (n != 0) recur(n / 2, row - n, col + n);
+            break;
+        case (true, false):
+            answer += n == 0 ? 2 : 2 * cnt;
+            if (n != 0) recur(n / 2, row + n, col - n);
+            break;
+        case (true, true):
+            answer += n == 0 ? 3 : 3 * cnt;
+            if (n != 0) recur(n / 2, row + n, col + n);
+            break;
     }
 }
