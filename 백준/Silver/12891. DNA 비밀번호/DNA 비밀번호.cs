401eff829b818da1b606364cbbc516e451885adc
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-23 14:13:21 +0900

    [Silver II] Title: DNA 비밀번호, Time: 220 ms, Memory: 22104 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cs
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.cs
@@ -5,23 +5,20 @@ var ACGT = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 var cnt = new int[4];
 int l = 0, r = input[1], answer = 0;
 
-var getCount = (string str, int n) =>
+var getCount = (char ch, int n) =>
 {
-    foreach(var c in str)
-    {
-        cnt[c switch { 'A' => 0, 'C' => 1, 'G' => 2, _ => 3 }] += n;
-    }
+    cnt[ch switch { 'A' => 0, 'C' => 1, 'G' => 2, _ => 3 }] += n;
 };
 
 var IsValid = (int[] arr) => ACGT.Zip(arr, (a, b) => a > b).Count(x => x == true) == 0;
 
-getCount(str[l..r], 1);
+foreach(var c in str[l..r]) getCount(c, 1);
 if (IsValid(cnt)) answer++;
 
 while(r < input[0])
 {
-    getCount(str[l++].ToString(), -1);
-    getCount(str[r++].ToString(), 1);
+    getCount(str[l++], -1);
+    getCount(str[r++], 1);
     if (IsValid(cnt)) answer++;
 }
 
