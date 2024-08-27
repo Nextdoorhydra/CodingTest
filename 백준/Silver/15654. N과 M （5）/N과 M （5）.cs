43d42111aa9f0839cda095b7ef49ecdf7c1c3240
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-27 18:27:18 +0900

    [Silver III] Title: N과 M (5), Time: 152 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cs
@@ -0,0 +1,21 @@
+using StreamWriter sw = new(Console.OpenStandardOutput());
+int rep = Array.ConvertAll(Console.ReadLine().Split(), int.Parse)[1];
+var seq = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+seq.Sort();
+back(0, 0, new List<int>());
+
+void back(int number, int depth, List<int> buf)
+{
+    if(depth == rep)
+    {
+        sw.WriteLine(string.Join(" ", buf));
+        return;
+    }
+
+    for(int i = 0; i < seq.Count; i++)
+    {
+        if (buf.Contains(seq[i])) continue;
+        back(seq[i], depth + 1, buf.Append(seq[i]).ToList());
+    }
+}
