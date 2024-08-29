2edc722527801198cac2f0ef525b72d242430fda
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-08-29 15:32:25 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4156 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    queue<int> que;
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        que.push(i);
+    }
+
+    while(que.size() > 1){
+        que.pop();
+        que.push(que.front());
+        que.pop();
+    }
+
+    cout << que.front();
+
+    return 0;
+}
