34282ab97ec150e087adbf6d2aaaed3ba581dda9
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-09-04 01:26:37 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 412 ms, Memory: 44520 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cs
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cs
@@ -1,13 +1,10 @@
 var NM = Console.ReadLine().Split().Select(int.Parse).ToList();
-int N = NM[0], M = NM[1], answer = int.MaxValue;
+int N = NM[0], M = NM[1], answer;
 
-var visited = Enumerable.Range(0, N)
-    .Select(_ => Enumerable.Range(0, M).Select(_ => false).ToList())
-    .ToList();
+var visit = new int[N, M, 2];
 var map = Enumerable.Range(0, N)
     .Select(_ => Console.ReadLine().Select(c => -int.Parse(c.ToString())).ToList())
     .ToList();
-
 
 var move = new Func<(int x, int y), (int x, int y)>[]
 {
@@ -17,83 +14,42 @@ var move = new Func<(int x, int y), (int x, int y)>[]
     pos => (pos.x, pos.y - 1)
 };
 var IsInRange = ((int x, int y) pos) => pos.x >= 0 && pos.y >= 0 && pos.x < N && pos.y < M;
-var to3tuple = ((int x, int y) v2, int z) => (v2.x, v2.y, z);
-var to2tuple = ((int x, int y, int z) v3) => (v3.x, v3.y);
 
-bfs(1);
+answer = N > 1 || M > 1 ? bfs() : 1;
+Console.WriteLine(answer switch { 0 => -1, _ => answer });
 
-void bfs(int boom)
+int bfs()
 {
-    Queue<(int x, int y)> queue = new();
-    Queue<(int x, int y)> queue2 = new();
-    queue.Enqueue((0, 0));
-    map[0][0] = 1;
-    visited[0][0] = true;
+    Queue<(int x, int y, int z)> queue = new();
+    queue.Enqueue((0, 0, 1));
+    visit[0, 0, 1] = 1;
 
-    while (queue.Count > 0)
+    while(queue.Count > 0)
     {
         var now = queue.Dequeue();
+        var time = visit[now.x, now.y, now.z];
 
-        foreach (var f in move)
+        foreach(var f in move)
         {
-            var next = f(now);
-
-            if (!IsInRange(next)) continue;
+            var next = f((now.x, now.y));
+            if (!IsInRange((next.x, next.y))) continue;
             var state = map[next.x][next.y];
 
-            if (state == 0)
+            if(next.x == N - 1 && next.y == M - 1)
             {
-                map[next.x][next.y] = map[now.x][now.y] + 1;
-                queue.Enqueue(next);
+                return time + 1;
             }
-            else if (state == -1)
+            else if(state == 0 && visit[next.x, next.y, now.z] == 0)
             {
-                visited[next.x][next.y] = true;
-                map[next.x][next.y] = map[now.x][now.y] + 1;
-                queue2.Enqueue(next);
+                visit[next.x, next.y, now.z] = time + 1;
+                queue.Enqueue((next.x, next.y, now.z));
             }
-        }
-    }
-
-    while (queue2.Count > 0)
-    {
-        var now = queue2.Dequeue();
-
-        foreach (var f in move)
-        {
-            var next = f(now);
-            if (!IsInRange(next) || visited[next.x][next.y]) continue;
-
-            var state = map[next.x][next.y];
-
-            if (state == 0)
+            else if(state == -1 && now.z > 0 && visit[next.x, next.y, 1] == 0)
             {
-                map[next.x][next.y] = map[now.x][now.y] + 1;
-                queue2.Enqueue(next);
-            }
-            else if (state != -1)
-            {
-                if(map[next.x][next.y] > map[now.x][now.y] + 1)
-                {
-                    map[next.x][next.y] = map[now.x][now.y] + 1;
-                    queue2.Enqueue(next);
-                }
+                visit[next.x, next.y, 0] = time + 1;
+                queue.Enqueue((next.x, next.y, 0));
             }
         }
     }
-
-    Console.WriteLine(map[^1][^1] > 0 ? map[^1][^1] : -1);
-}
-
-void display()
-{
-    Console.WriteLine();
-    for (int i = 0; i < map.Count; i++)
-    {
-        for (int j = 0; j < map[i].Count; j++)
-        {
-            Console.Write($"{map[i][j],2}");
-        }
-        Console.WriteLine();
-    }
+    return 0;
 }
