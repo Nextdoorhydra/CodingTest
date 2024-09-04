177a56c79fe5c3795010748950c470ab073b583b
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-09-05 03:52:03 +0900

    [Gold IV] Title: 트리의 지름, Time: 76 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.cs
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cs
@@ -9,52 +9,47 @@ for(int i = 0; i < rep - 1; i++)
     Tree[children].AddEdge(Tree[parent], weight);
 }
 
-tree.FindMaxRadius(Tree);
+tree.printMaxRadius(Tree);
 
 class tree
 {
     public int id;
-    public List<edge> edgeList;
-    public static int answer = 0;
-    public static HashSet<int> visited = new HashSet<int>();
+    public List<edge> edgeList = new();
+    public List<int> maxWeights = new();
+    public static HashSet<int> visited = new();
 
-    public tree(int id)
+    public tree(int id) => this.id = id;
+
+    public void AddEdge(tree To, int weight) => edgeList.Add(new edge(To, weight));
+
+    public static void printMaxRadius(tree[] Tree)
     {
-        this.id = id;
-        edgeList = new List<edge>();
+        setAdjacencyDistance(Tree[1], 0);
+        int max = 0;
+        foreach(var node in Tree)
+            max = Math.Max(node.maxWeights.Take(2).Sum(), max);
+        Console.WriteLine(max);
     }
 
-    public void AddEdge(tree To, int weight)
+    public static int setAdjacencyDistance(tree node, int radius)
     {
-        edgeList.Add(new edge(To, weight));
-    }
+        visited.Add(node.id);
+        node.maxWeights.AddRange(new[] { 0, 0 } );
 
-    public static void FindMaxRadius(tree[] Tree)
-    {
-        foreach(var tree in Tree.Skip(1))
+        foreach(var next in node.edgeList)
         {
-            DFS(tree, 0);
-            visited.Clear();
+            if(!visited.Contains(next.To.id))
+            {
+                node.maxWeights.Add(setAdjacencyDistance(next.To, next.weight));
+            }
         }
-        Console.WriteLine(answer);
-    }
-
-    public static void DFS(tree root, int radius)
-    {
-        visited.Add(root.id);
-
-        foreach (var next in root.edgeList)
-        {
-            if (!visited.Contains(next.To.id)) 
-                DFS(next.To, next.weight + radius);
-        }
-
-        answer = Math.Max(answer, radius);
+        node.maxWeights.Sort((x, y) => y.CompareTo(x));
+        return node.maxWeights[0] + radius;
     }
 
     public class edge
     {
-        public int weight;
+        public int weight { get; private set; }
         public tree To;
 
         public edge(tree To, int weight)
