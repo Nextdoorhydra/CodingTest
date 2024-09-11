a207842438331a66a04c68bf0db88d5d86b53e8e
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-09-11 16:15:50 +0900

    [Gold V] Title: 다각형의 면적, Time: 64 ms, Memory: 9220 KB -BaekjoonHub

--- 백준/Gold/2166. 다각형의 면적/다각형의 면적.cs
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.cs
@@ -1,6 +1,2 @@
-var CCW = (List<double> A, List<double> B) => 0.5 * (A[1] * B[2] - A[2] * B[1] + A[2] * B[0] - A[0] * B[2] + A[0] * B[1] - A[1] * B[0]);
-
-var input = Enumerable.Range(1, int.Parse(Console.ReadLine()))
-    .Select(x => Console.ReadLine().Split().Select(double.Parse).Append(0).ToList()).ToList();
-var ls = input.Skip(1).Select(x => x.Zip(input[0], (x, y) => x - y).ToList()).ToList();
-Console.WriteLine($"{Math.Abs(ls.Skip(1).Aggregate((pre: ls[0], sum: 0d), (acc, x) => (x, acc.sum + CCW(x, acc.pre))).sum):F1}");
+var A = Enumerable.Range(1, int.Parse(Console.ReadLine())).Select(x => Console.ReadLine().Split().Select(double.Parse).ToList()).ToList();
+Console.WriteLine($"{Math.Abs(A.Append(A[0]).Aggregate((S: 0d, v: (List<double>)null), (pre, v) => pre.v == null ? (0d, v) : (pre.S + 0.5 * (pre.v[0] * v[1] - pre.v[1] * v[0]), v)).S):F1}");
