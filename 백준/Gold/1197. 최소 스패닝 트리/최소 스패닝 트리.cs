1f9c16bb3c39c90728157426dca6a748e5627c3b
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-09-30 18:08:27 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 192 ms, Memory: 28048 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cs
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cs
@@ -1,32 +1,22 @@
 var input = () => Console.ReadLine().Split().Select(int.Parse).ToList();
-var A = input();
-int V = A[0], E = A[1];
+var A = input(); int V = A[0], E = A[1];
 List<long> MST = new();
-List<int> set = Enumerable.Range(0, V + 1).ToList();
-List<List<int>> edgeList = Enumerable.Range(0, E).Select(_ => input()).ToList().OrderBy(x => x.Last()).ToList();
+var set = Enumerable.Range(0, V + 1).ToList();
+var edgeList = Enumerable.Range(0, E).Select(_ => input()).ToList().OrderBy(x => x.Last()).ToList();
 
 int find(int x) => set[x] == x ? x : set[x] = find(set[x]);
-bool union(int x, int y) 
-{ 
-    int a = find(x), b = find(y);
-    if(a != b)
-    {
-        set[a] = b;
-        return true;
-    } 
-    return false;
-}
+var union = (int x, int y) => find(x) != find(y) ? (set[find(x)] = find(y), true) : (0, false);
 
-while(true)
+while (true)
 {
-    foreach(var edge in edgeList)
+    foreach (var edge in edgeList)
     {
-        if(MST.Count == V - 1)
+        if (MST.Count == V - 1)
         {
             Console.WriteLine(MST.Sum());
             return;
         }
 
-        if (union(edge[0], edge[1])) MST.Add(edge[2]);
+        if (union(edge[0], edge[1]).Item2) MST.Add(edge[2]);
     }
 }
