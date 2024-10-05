9fef24ea932f683f7f66f3ca49cd777342b50a2e
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-10-05 23:53:12 +0900

    [Gold IV] Title: 행렬 제곱, Time: 68 ms, Memory: 6896 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cs
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cs
@@ -2,24 +2,20 @@ using static System.Linq.Enumerable;;
 var read = () => Console.ReadLine().Split().Select(long.Parse).ToList();
 var input = read();
 var m = Range(0, (int)input[0]).Select(_ => read()).ToList();
-int size = (int)input[0]; long n = input[1];
-var I = Range(0, size).Select(i => Range(0, size).Select(j => i == j ? 1l : 0l).ToList()).ToList();
+int L = (int)input[0];
+var I = Range(0, L).Select(i => Range(0, L).Select(j => i == j ? 1l : 0l).ToList()).ToList();
 
-var dotProduct = (IEnumerable<long> rVec, IEnumerable<long> cVec) => rVec.Zip(cVec, (x, y) => x * y).Sum();
 var mul = (List<List<long>> ma, List<List<long>> mb) =>
 {
-    var mab = Range(0, size).Select(_ => new List<long>()).ToList();
-    for (int r = 0; r < size; r++)
-        for (int c = 0; c < size; c++)
-            mab[r].Add(dotProduct(ma[r], mb.Select(vec => vec.Skip(c).First())) % 1000);
+    var mab = Range(0, L).Select(_ => new List<long>()).ToList();
+    for (int r = 0; r < L; r++) for (int c = 0; c < L; c++) mab[r].Add(ma[r].Zip(mb.Select(vec => vec.Skip(c).First()), (x, y) => x * y).Sum() % 1000);
     return mab;
 };
 
-while (n > 0)
+for(var n = input[1]; n > 0 ; n /= 2)
 {
     if (n % 2 == 1) I = mul(I, m);
     m = mul(m, m);
-    n /= 2;
 }
 
 foreach (var vec in I) Console.WriteLine(string.Join(" ", vec));
