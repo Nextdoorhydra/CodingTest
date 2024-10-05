508cce34cd7cc9464ecce56dc626d48f63f6a502
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-10-05 23:37:55 +0900

    [Gold IV] Title: 행렬 제곱, Time: 68 ms, Memory: 6900 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.cs
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.cs
@@ -1,28 +1,25 @@
-using StreamWriter sw = new(Console.OpenStandardOutput());
+using static System.Linq.Enumerable;;
 var read = () => Console.ReadLine().Split().Select(long.Parse).ToList();
-var printMatrix = (List<List<long>> m) => { foreach (var vec in m) sw.WriteLine(string.Join(" ", vec)); };
 var input = read();
-printMatrix(matrixPower(Enumerable.Range(0, (int)input[0]).Select(_ => read()).ToList(), (int)input[0], input[1]));
+var m = Range(0, (int)input[0]).Select(_ => read()).ToList();
+int size = (int)input[0]; long n = input[1];
+var I = Range(0, size).Select(i => Range(0, size).Select(j => i == j ? 1l : 0l).ToList()).ToList();
 
-List<List<long>> matrixPower(List<List<long>> m, int size, long n)
+var dotProduct = (IEnumerable<long> rVec, IEnumerable<long> cVec) => rVec.Zip(cVec, (x, y) => x * y).Sum();
+var mul = (List<List<long>> ma, List<List<long>> mb) =>
 {
-    var I = Enumerable.Range(0, size).Select(i => Enumerable.Range(0, size).Select(j => i == j ? 1l : 0l).ToList()).ToList();
+    var mab = Range(0, size).Select(_ => new List<long>()).ToList();
+    for (int r = 0; r < size; r++)
+        for (int c = 0; c < size; c++)
+            mab[r].Add(dotProduct(ma[r], mb.Select(vec => vec.Skip(c).First())) % 1000);
+    return mab;
+};
 
-    var mul = (List<List<long>> ma, List<List<long>> mb) =>
-    {
-        var mab = Enumerable.Range(0, size).Select(_ => new List<long>()).ToList();
-        var dotProduct = (IEnumerable<long> rVec, IEnumerable<long> cVec) => rVec.Zip(cVec, (x, y) => x * y).Sum();
-        for (int r = 0; r < size; r++)
-            for (int c = 0; c < size; c++)
-                mab[r].Add(dotProduct(ma[r], mb.Select(vec => vec.Skip(c).First())) % 1000);
-        return mab;
-    };
-    
-    while (n > 0)
-    {
-        if (n % 2 == 1) I = mul(I, m);
-        m = mul(m, m);
-        n /= 2;
-    }
-    return I;
+while (n > 0)
+{
+    if (n % 2 == 1) I = mul(I, m);
+    m = mul(m, m);
+    n /= 2;
 }
+
+foreach (var vec in I) Console.WriteLine(string.Join(" ", vec));
