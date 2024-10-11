a3ca7e22b560289e2b18e6cb2e23ecc5b2edfa1b
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-10-11 10:21:32 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1536 ms, Memory: 128916 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cs
@@ -0,0 +1,12 @@
+var input = read();
+int N = input[0], M = input[1], ANSWER = 0, latest = 0;
+var parent = Enumerable.Range(0, N + 1).ToList();
+var edges = Enumerable.Range(0, M).Select(_ => read()).OrderBy(x => x.Last()).ToList();
+
+foreach(var edge in edges) if (union(edge[0], edge[1]) != -1) ANSWER += (latest = edge[2]);
+
+Console.WriteLine(ANSWER - latest);
+
+List<int> read() => Console.ReadLine().Split().Select(int.Parse).ToList();
+int find(int x) => x == parent[x] ? x : parent[x] = find(parent[x]);
+int union(int x, int y) => find(x) == find(y) ? -1 : parent[find(y)] = find(x);
