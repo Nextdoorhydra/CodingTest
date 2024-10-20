a909d120bd9f0119226620791eed2d4273ddaed6
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-10-21 04:18:19 +0900

    [Gold III] Title: 게임 개발, Time: 72 ms, Memory: 10004 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.cs
+++ 백준/Gold/1516. 게임 개발/게임 개발.cs
@@ -1,14 +1,12 @@
 var read = () => Console.ReadLine().Split().Select(int.Parse).ToList();
-
-var input = read();
-int N = input[0], now;
-List<int> inorder = Enumerable.Repeat(0, N + 1).ToList(),
-    lapsed = new List<int>(inorder), times = new List<int>(inorder);
+Queue<int> queue = new Queue<int>();
+int N = read()[0], now;
+List<int> inorder = Enumerable.Repeat(0, N + 1).ToList(), lapsed = inorder.ToList(), times = inorder.ToList();
 var edgeList = inorder.Select(_ => new List<int>()).ToList();
 
-for(int i = 1; i <= N; i++)
+for (int i = 1; i <= N; i++)
 {
-    input = read();
+    var input = read();
     times[i] = input[0];
     foreach (var next in input.Take(input.Count() - 1).Skip(1))
     {
@@ -17,10 +15,9 @@ for(int i = 1; i <= N; i++)
     }
 }
 
-Queue<int> queue = new Queue<int>();
-for(int i = 1; i <= N; i++) if(inorder[i] == 0) queue.Enqueue(i);
+for (int i = 1; i <= N; i++) if (inorder[i] == 0) queue.Enqueue(i);
 
-while(queue.Count > 0)
+while (queue.Count > 0)
 {
     now = queue.Dequeue();
     times[now] += lapsed[now];
