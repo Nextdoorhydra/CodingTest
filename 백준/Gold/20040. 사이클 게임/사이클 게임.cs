48d8c9e90a3b27568ceb1dff986682a73d927677
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-10-24 16:36:19 +0900

    [Gold IV] Title: 사이클 게임, Time: 1116 ms, Memory: 106860 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cs
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cs
@@ -1,21 +1,16 @@
 var read = () => Console.ReadLine().Split().Select(int.Parse).ToList();
 var input = read();
-int N = input[0], M = input[1], turn = 0;
+int N = input[0], M = input[1], turn = 0, flag = 0;
 var edgeList = Enumerable.Range(0, M).Select(_ => read()).ToList();
-
 var parent = Enumerable.Range(0, N + 1).ToList();
-int find(int x) => x == parent[x] ? x : find(parent[x]);
-int union(int x, int y) => find(x) == find(y) ? -1 : parent[find(y)] = find(x);
 
-foreach(var edge in edgeList)
+foreach (var edge in edgeList)
 {
     turn++;
-
-    if (union(edge.First(), edge.Last()) == -1)
-    {
-        Console.WriteLine(turn);
-        return;
-    }
+    if (union(edge.First(), edge.Last()) == -1) { flag = 1; break; }
 }
 
-Console.WriteLine(0);
+Console.WriteLine(flag < 1 ? 0 : turn);
+
+int find(int x) => x == parent[x] ? x : parent[x] = find(parent[x]);
+int union(int x, int y) => find(x) == find(y) ? -1 : parent[find(y)] = find(x);
