9380d59b09c30a92e9bb7a3a7c076d49a1269aee
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-11-13 02:29:43 +0900

    [Bronze II] Title: Hashing, Time: 56 ms, Memory: 5700 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cs
+++ 백준/Bronze/15829. Hashing/Hashing.cs
@@ -1,1 +1,4 @@
-Console.ReadLine();Console.WriteLine(Console.ReadLine().ToCharArray().Select((x, i) => (x - 'a' + 1) * Math.Pow(31, i)).Sum() % 1234567891);
+Console.ReadLine();
+Console.WriteLine(Console.ReadLine().ToCharArray()
+    .Select((x, i) => (x - 'a' + 1) * Enumerable.Repeat(31, i)
+    .Aggregate(1L, (pre, x) => (pre * x) % 1234567891)).Sum() % 1234567891);
