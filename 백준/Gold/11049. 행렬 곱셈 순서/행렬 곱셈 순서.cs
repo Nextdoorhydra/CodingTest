0137a781507323851d01af523412a19ac12e2e6f
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-11-13 14:25:16 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 176 ms, Memory: 7244 KB -BaekjoonHub

---
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.cs
@@ -0,0 +1,21 @@
+var read = () => Console.ReadLine().Split().Select(int.Parse).ToList();
+int N = read()[0];
+var m = Enumerable.Range(0, N).Select(_ => read()).ToList();
+var dp = new int[N, N];
+
+for (int i = 0; i + 1 < N; i++)
+    dp[i, i + 1] = m[i][0] * m[i][1] * m[i + 1][1];
+
+for (int i = 1; i < N; i++)
+    for (int j = 0; j + i + 1 < N; j++)
+    {
+        int min = dp[j, j + i] + m[j][0] * m[j + i][1] * m[j + i + 1][1];
+
+        for (int k = 1; k <= i; k++)
+            min = Math.Min(min, m[j][0] * m[j + k - 1][1] * m[j + i + 1][1] 
+                + dp[j + k, j + i + 1] + (k - 1 == 0 ? 0 : dp[j, j + k - 1]));
+
+        dp[j, j + i + 1] = min;
+    }
+
+Console.WriteLine(dp[0, N - 1]);
