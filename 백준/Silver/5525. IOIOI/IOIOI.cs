d27bff5da3f52a99b3b2a2ed53399e59b5c21710
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2024-11-15 22:05:29 +0900

    [Silver I] Title: IOIOI, Time: 88 ms, Memory: 17948 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cs
+++ 백준/Silver/5525. IOIOI/IOIOI.cs
@@ -2,32 +2,26 @@ var read = () => Console.ReadLine();
 int N = int.Parse(read()), M = int.Parse(read());
 var str = read();
 var pattern = new string(Enumerable.Range(1, 2 * N + 1).Select(x => x % 2 != 0 ? 'I' : 'O').ToArray());
+var pos = Enumerable.Range(1, pattern.Length).Select(x => x % 2 != 0 ? 0 : 1).ToList();
 
-int ANS = 0;
+int ANS = 0, idx = 0;
 
-for(int i = 0; i <= M - pattern.Length; i++)
+for(int i = 0; i < M; i++)
 {
-    if (match(str, pattern, i))
+    if (str[i] == pattern[idx])
     {
-        i++;
-        ANS++;
+        idx++;
+
+        if(idx == pattern.Length)
+        {
+            ANS++;
+            idx -= 2;
+        }
+    }
+    else
+    {
+        idx = pos[idx];
     }
 }
 
 Console.WriteLine(ANS);
-
-bool match(string s, string p, int idx)
-{
-    bool flag = true;
-
-    for(int i = 0; i < p.Length; i++)
-    {
-        if (s[idx + i] != p[i])
-        {
-            flag = false;
-            break;
-        }
-    }
-
-    return flag;
-}
