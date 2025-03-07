7fb7ef9ac0f9546438b044dffcaf04d0fbe28cbc
Nextdoorhydra <151500823+Nextdoorhydra@users.noreply.github.com>
2025-03-08 06:39:52 +0900

    [Platinum V] Title: 볼록 껍질, Time: 204 ms, Memory: 33504 KB -BaekjoonHub

--- 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cs
+++ 백준/Platinum/1708. 볼록 껍질/볼록 껍질.cs
@@ -1,5 +1,3 @@
-using System.Runtime.Intrinsics;
-
 int rep = int.Parse(Console.ReadLine());
 var v = new List<(long x, long y)>();
 var pripara = new Stack<(long x, long y)>();
@@ -13,45 +11,33 @@ for (int i = 0; i < rep; i++)
 
 v = v.OrderBy(v => v.y).ThenBy(v => v.x).ToList();
 var v0 = v[0];
-v = v.Skip(1).OrderBy(v => (v.x - v0.x, v.y - v0.y), new AngleComparer()).ToList();
+v = v.Skip(1).OrderBy(v => (v.x - v0.x, v.y - v0.y), new AngleComparer())
+    .ThenBy(v => v.y)
+    .ThenBy(v => v.x)
+    .ToList();
+
+var ccw = ((long x, long y) v1, (long x, long y) v2, (long x, long y) v3)
+    => v1.x * v2.y + v2.x * v3.y + v3.x * v1.y - v2.x * v1.y - v3.x * v2.y - v1.x * v3.y;
 
 pripara.Push(v0);
 pripara.Push(v[0]);
 
-var ccw = ((long x, long y) v1, (long x, long y) v2, (long x, long y) v3)
-    => v1.x * v2.y + v2.x * v3.y + v3.x * v1.y - v2.x * v1.y - v3.x * v2.y - v1.x * v3.y;
-var norm =((long x, long y) v) => v.x * v.x + v.y * v.y;
-
-for (int i = 1; i < v.Count; i++)
+for(int i = 1; i < rep - 1; i++)
 {
-    var v2 = pripara.Pop();
-    var v1 = pripara.Pop();
-    var v3 = v[i];
-    pripara.Push(v1);
+    while(pripara.Count >= 2)
+    {
+        var laala = pripara.Pop();
+        var mirei = pripara.Peek();
 
-    var det = ccw(v1, v2, v3);
+        if (ccw(v[i], laala, mirei) < 0)
+        {
+            pripara.Push(laala);
+            break;
+        }
+    }
 
-    if (det > 0)
-    {
-        pripara.Push(v2);
-        pripara.Push(v3);
-    }
-    else if (det < 0)
-    {
-        i--;
-    }
-    else
-    {
-        (long x, long y) dv2 = (v2.x - v1.x, v2.y - v1.y);
-        (long x, long y) dv3 = (v3.x - v1.x, v3.y - v1.y);
-        pripara.Push(norm(dv2) > norm(dv3) ? v2 : v3);
-    }
+    pripara.Push(v[i]);
 }
-
-var last = pripara.Pop();
-if (ccw(v0, last, pripara.Peek()) != 0)
-    pripara.Push(last);
-
 
 Console.WriteLine(pripara.Count);
 
@@ -59,20 +45,8 @@ public class AngleComparer : IComparer<(long x, long y)>
 {
     public int Compare((long x, long y) v1, (long x, long y) v2)
     {
-        var det = v1.x * v2.y - v1.y * v2.x;
+        var det = v1.y * v2.x - v1.x * v2.y;
 
-        if (det > 0)
-        {
-            return -1;
-        }
-        else if (det == 0)
-        {
-            if(v1.y != v2.y) return v1.y < v2.y ? -1 : 1;
-            else return v1.x < v2.x ? -1 : 1;
-        }
-        else
-        {
-            return 1;
-        }
+        return Math.Sign(det);
     }
 }
