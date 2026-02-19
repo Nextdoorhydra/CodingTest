fcf60ebbd9512571eee1eb5c283d9672e4bf06d8
NDH <151500823+Nextdoorhydra@users.noreply.github.com>
2026-02-19 11:25:13 +0900

    [Gold IV] Title: 연구소, Time: 2944 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,35 +1,35 @@
-import sys, enum, itertools
+import sys, itertools
 
 # declaration
 input = sys.stdin.readline
-class Cell(enum.IntEnum):
-    SAFE = 0
-    WALL = 1
-    VIRUS = 2    
-answer = 0
+SAFE, WALL, VIRUS = 0, 1, 2 
+answer, initSafe = 0, 0
 dr, dc = [-1, 1, 0, 0], [0, 0, -1, 1]
-lab, sample, virus = [], [], []
-countSafe = lambda lab: sum(row.count(Cell.SAFE) for row in lab)
-isSpreadable = lambda r, c: 0 <= r < N and 0 <= c < M and lab[r][c] == Cell.SAFE
+lab, sample, virus, spreadList = [], [], [], []
+countSafe = lambda lab: sum(row.count(SAFE) for row in lab)
+isSpreadable = lambda r, c: 0 <= r < N and 0 <= c < M and lab[r][c] == SAFE
 
 # input
 N, M = map(int, input().split())
-lab = [list(map(lambda x: Cell(int(x)), input().split())) for _ in range(N)]
+lab = [list(map(int, input().split())) for _ in range(N)]
 
 # logic
 for r in range(N):
     for c in range(M):
-        if lab[r][c] == Cell.VIRUS:
+        if lab[r][c] == VIRUS:
             virus.append((r, c))
-        elif lab[r][c] == Cell.SAFE:
+        elif lab[r][c] == SAFE:
             sample.append((r, c))
+            initSafe += 1
 
 for walls in itertools.combinations(sample, 3):
     # sample 3 walls
     for r, c in walls:
-        lab[r][c] = Cell.WALL
+        lab[r][c] = WALL
 
     # spread virus
+    spreadList.clear()
+    
     for r, c in virus:
         stack = [(r, c)]
         while stack:
@@ -37,18 +37,17 @@ for walls in itertools.combinations(sample, 3):
             for i in range(4):
                 nr, nc = r + dr[i], c + dc[i]
                 if isSpreadable(nr, nc):
-                    lab[nr][nc] = Cell.VIRUS
+                    lab[nr][nc] = VIRUS
                     stack.append((nr, nc))
+                    spreadList.append((nr, nc))
             
-    answer = max(answer, countSafe(lab))   
+    answer = max(answer, initSafe - len(spreadList) - 3)
 
     # reset lab
-    for r in range(N):
-        for c in range(M):
-            if lab[r][c] == Cell.VIRUS and (r, c) not in virus:
-                lab[r][c] = Cell.SAFE
-            elif (r, c) in walls:
-                lab[r][c] = Cell.SAFE
+    for r, c in spreadList:
+        lab[r][c] = SAFE
+    for r, c in walls:
+        lab[r][c] = SAFE
 
 # output
 print(answer)
