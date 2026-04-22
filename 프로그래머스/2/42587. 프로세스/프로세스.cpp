4a0bd5276e56965c4e9524ee71b72cc5e52bc266
NDH <151500823+Nextdoorhydra@users.noreply.github.com>
2026-04-22 10:48:08 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+int solution(vector<int> priorities, int location) {
+    // ----------init----------
+    struct element {
+        int index, priority;
+    };
+    int i, idx = 0, answer = 1;
+    element e;
+    queue<element> q;
+    priority_queue<int> pq;
+
+    // ----------processing----------
+    for (int x : priorities) q.push(element{ idx++, x });
+    for (int x : priorities) pq.push(x);
+
+    // ----------main----------
+    vector<element> debug;
+
+    while(!q.empty()) {
+        e = q.front();
+        q.pop();
+
+        if (e.priority >= pq.top()) {
+            if (e.index == location)
+                return answer;
+            
+            answer++;
+            pq.pop();
+        }
+        else {
+            q.push(e);
+        }        
+    }
+
+    return answer;
+}
