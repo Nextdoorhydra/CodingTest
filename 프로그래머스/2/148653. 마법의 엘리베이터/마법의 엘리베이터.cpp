ce45f5ad70066b889c534f79542c8bd2515243ae
NDH <151500823+Nextdoorhydra@users.noreply.github.com>
2026-04-28 13:47:53 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <string>
+#include <vector>
+
+using namespace std;
+vector<int> step = { 1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000, 1'000'000'000 };
+
+int dp(int start, int storey, int val = 0) {
+    if (start < 0)
+        return 0;
+
+    val += storey / step[start];
+
+    int nextStorey = storey % step[start];
+    int nextStorey2 = abs(nextStorey - step[start]);
+
+    val += min(dp(start - 1, nextStorey), dp(start - 1, nextStorey2, 1));
+
+    return val;
+}
+
+int solution(int storey) {
+    int answer = 0;
+    int start = -1;
+    int direction = -1;
+    
+    for (int i = step.size() - 1; i >= 0; --i) {
+        if (storey < step[i])
+            continue;
+        start = i + 1;
+        break;
+    }
+
+    answer = dp(start, storey);
+
+    return answer;
+}
