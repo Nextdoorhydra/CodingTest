8b479e3d7832562a0c2323b679f8d3483087a4e5
NDH <151500823+Nextdoorhydra@users.noreply.github.com>
2026-04-30 10:23:19 +0900

    [level 2] Title: 할인 행사, Time: 37.69 ms, Memory: 9.82 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> want, vector<int> number, vector<string> discount) {
+    // 선언
+    vector<int> vn;
+    int answer = 0;
+    map<std::string, int> m;
+
+    // pre-process
+    for (int i = 0; i < want.size(); ++i) {
+        m.insert({ want[i], i });
+    }
+
+    // main
+    for (int i = 0; i + 10 <= discount.size(); ++i) {
+        vn = number;
+
+        for (int j = i; j < i + 10; ++j) {
+            std::string key = discount[j];
+
+            if (m.find(key) != m.end()) {
+                int idx = m[key];
+
+                vn[idx] -= vn[idx] > 0 ? 1 : 0;
+            }
+        }
+
+        if (std::all_of(vn.begin(), vn.end(), [](int n) { return n == 0; })) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
