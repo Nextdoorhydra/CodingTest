dd00efe4ae0339f5dc56dd471ab393afb3d467ca
NDH <151500823+Nextdoorhydra@users.noreply.github.com>
2026-06-01 06:04:17 +0900

    [level 2] Title: 점 찍기, Time: 2.74 ms, Memory: 3.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+long long solution(int k, int d)
+{
+    long long answer = 0;
+    long long llk = k, lld = d;
+
+    auto getC = [=](long long x) -> long long {
+        return floor(sqrt(lld * lld - x * x));
+        };
+
+    for (long long r = 0; r <= lld; r += llk)
+    {
+        answer += getC(r) / llk + 1;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution(2, 4);
+    return 0;
+}
